c29bd94fdbce398c6e0ae8f0be0e75823a8ffd8c
vlad <FreeMooont@gmail.com>
2021-06-24 21:11:47 +0800

    Dev init

--- src/Components/CourseSlide/CourseSlide.jsx
+++ src/Components/CourseSlide/CourseSlide.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 function CourseSlide({courseNumber, program}){
+    console.log('Develop branch');
     return(
         <section className="section course--slide">
             <h1>{courseNumber} курс</h1>

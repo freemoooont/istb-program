8615fe42c81ef12fb7913c6c8fa1923ac7494785
vlad <FreeMooont@gmail.com>
2021-06-24 22:18:59 +0800

    Добавляет слайды вправо

--- src/Components/CourseSlide/CourseSlide.jsx
+++ src/Components/CourseSlide/CourseSlide.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import MainSlide from "./MainSlide/MainSlide";
+import RightSlide from "./RightSlide/RightSlide";
 
-function CourseSlide({courseNumber, program}){
+function CourseSlide(props){
     console.log('Develop branch');
     return(
         <section className="section course--slide">
-            <h1>{courseNumber} курс</h1>
+            <div className="slide">
+                <MainSlide {...props}/>
+            </div>
+            <div className="slide">
+                <RightSlide {...props}/>
+            </div>
+
         </section>
     )
 }
---
+++ src/Components/CourseSlide/MainSlide/MainSlide.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MainSlide({courseNumber, program}){
+
+    return(
+        <h1>
+            {courseNumber} курс
+        </h1>
+    )
+}
+
+export default MainSlide;

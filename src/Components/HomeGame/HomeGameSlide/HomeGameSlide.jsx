7d85f485f9645e3422df175561da2151e497af81
vlad <FreeMooont@gmail.com>
2021-06-27 04:29:37 +0800

    Анимации бургера и логика анимации менюшки

--- src/Components/HomeGame/HomeGameSlide/HomeGameSlide.jsx
+++ src/Components/HomeGame/HomeGameSlide/HomeGameSlide.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './homegameslide.css';
 import hand from "../../../assets/hand.svg"
-import burger from "../../../assets/Hamburger 2.svg";
 import all from "../../../assets/vector-all.svg";
 import { useSpring, animated } from 'react-spring'
 
 
 function HomeGameSlide() {
-    const styles = useSpring({
+    const stickStyles = useSpring({
         loop: true,
         to: [
             { opacity: 1, color: '#ffaaee', transform: `translate(0px, -30px)` },
@@ -15,6 +14,29 @@ function HomeGameSlide() {
         ],
         from: { opacity: 0, color: 'red', transform: `translate(0px, 0px)` },
     })
+
+    const [isOpen, setIsOpen] = React.useState(false);
+
+    const first = useSpring({
+        transform: isOpen
+            ? "translate(5px, 32px) rotate(-45deg)"
+            : "translate(2px, 7px) rotate(0deg)"
+    });
+    const second = useSpring({
+        transform: isOpen
+            ? "translate(10px, 4px) rotate(45deg)"
+            : "translate(2px, 19px) rotate(0deg)"
+    });
+    const third = useSpring({
+        transform: isOpen
+            ? "translate(5px, 32px) rotate(-45deg)"
+            : "translate(2px, 31px) rotate(0deg)"
+    });
+
+    const { x } = useSpring({
+        x: isOpen ? 0 : 100
+    })
+
     return (
         <>
             <div className="home-wrapper h-100">
@@ -23,7 +45,18 @@ function HomeGameSlide() {
                     <div className="container h-100 container--header">
                         <header className="welcome--header">
                             <span className="welcome--logo">ИИТИАД</span>
-                            <img src={burger} alt="" className="welcome--burger" />
+                            <svg
+                                onClick={() => setIsOpen(!isOpen)}
+                                width="40"
+                                height="32"
+                                viewBox="0 0 44 44"
+                                fill="#fafafa"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <animated.rect width="40" height="4" rx="2" style={first} />
+                                <animated.rect width="40" height="4" rx="2" style={second} />
+                                <animated.rect width="40" height="4" rx="2" style={third} />
+                            </svg>
                         </header>
 
                         <div className="welcome--info">
@@ -35,8 +68,14 @@ function HomeGameSlide() {
                         </div>
                     </div>
 
+                    {/*Вот здесь блок с меню, который надо воткнуть нормально*/}
+                    <animated.div
+                        style={{ transform: x.interpolate(x => `translate3d(${x* -1}%, 0, 0)`)}}
+                        className="welcome--menu__test"
+                    />
+
                     <div className="hand--icon--wrapper">
-                        <animated.img style={styles} className="hand--icon" src={hand}/>
+                        <animated.img style={stickStyles} className="hand--icon" src={hand}/>
                     </div>
                 </div>
             </div>

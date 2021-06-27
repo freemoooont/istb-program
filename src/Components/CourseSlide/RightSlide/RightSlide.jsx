00bdea37433d1fbbb450ed8276840d6bbf9bbb59
Neytllas <looopit@gmail.com>
2021-06-27 13:34:51 +0800

    добавление блоков для предметов

--- src/Components/CourseSlide/RightSlide/RightSlide.jsx
+++ src/Components/CourseSlide/RightSlide/RightSlide.jsx
@@ -1,22 +1,50 @@
 import React from 'react';
 import './rightslide.css';
 
-function RightSlide({subjects}) {
+function RightSlide({ subjects }) {
     return (
         <>
             <div className="wrapper">
-                <h2>
-                    Пацанский график
-                </h2>
-                <ul>
-                    {
-                        subjects.map(
-                            (item,idx) =>
-                                <li key={idx} >
-                                    {item.title}
-                                </li>)
-                    }
-                </ul>
+                <div className="container">
+                    <div className="text--semestr">Семестр</div>
+
+                    <div className="row align-items-center row--subject">
+                        <div className="col-auto">
+                            <div className="col--subject text--subject">
+                                Математика
+                            </div>
+                        </div>   
+                    </div>
+
+                    <div className="row align-items-center row--subject">
+                        <div className="col-auto">
+                            <div className="col--subject text--subject">
+                                Иностранный язык
+                            </div>
+                        </div>   
+                    </div>
+
+                    <div className="row align-items-center row--subject">
+                        <div className="col-auto">
+                            <div className="col--subject text--subject">
+                                Программирование 
+                            </div>
+                        </div>   
+                    </div>
+
+                    
+
+                    <ul>
+                        {
+                            subjects.map(
+                                (item, idx) =>
+                                    <li key={idx} >
+                                        {item.title}
+                                    </li>)
+                        }
+                    </ul>
+                </div>
+
             </div>
         </>
     )

935b68b940ed9576f29cdb776cb8412cd262df01
vlad <FreeMooont@gmail.com>
2021-06-27 14:22:38 +0800

    Предметы выводятся в цвет

--- src/Components/CourseSlide/RightSlide/RightSlide.jsx
+++ src/Components/CourseSlide/RightSlide/RightSlide.jsx
@@ -3,50 +3,23 @@ import './rightslide.css';
 
 function RightSlide({ subjects }) {
     return (
-        <>
-            <div className="wrapper">
-                <div className="container">
-                    <div className="text--semestr">Семестр</div>
-
-                    <div className="row align-items-center row--subject">
-                        <div className="col-auto">
-                            <div className="col--subject text--subject">
-                                Математика
-                            </div>
-                        </div>   
-                    </div>
-
-                    <div className="row align-items-center row--subject">
-                        <div className="col-auto">
-                            <div className="col--subject text--subject">
-                                Иностранный язык
-                            </div>
-                        </div>   
-                    </div>
-
-                    <div className="row align-items-center row--subject">
-                        <div className="col-auto">
-                            <div className="col--subject text--subject">
-                                Программирование 
-                            </div>
-                        </div>   
-                    </div>
-
-                    
-
-                    <ul>
-                        {
-                            subjects.map(
-                                (item, idx) =>
-                                    <li key={idx} >
+        <div className="wrapper">
+            <div className="container">
+                <div className="text--semestr">Семестр</div>
+                {
+                    subjects.map(
+                        (item, idx) => (
+                            <div key={idx} className="row align-items-center row--subject">
+                                <div className="col-auto">
+                                    <div className="col--subject text--subject">
                                         {item.title}
-                                    </li>)
-                        }
-                    </ul>
-                </div>
-
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    )}
             </div>
-        </>
+        </div>
     )
 }
 

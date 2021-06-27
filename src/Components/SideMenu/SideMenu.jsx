ef6154fcdc2c103331064b4b161c59b2156ad4d0
vlad <FreeMooont@gmail.com>
2021-06-27 14:13:50 +0800

    Компонент сайд меню

---
+++ src/Components/SideMenu/SideMenu.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './sidemenu.css';
+
+function SideMenu(){
+    return(
+        <div>
+            <h1> кчао </h1>
+        </div>
+    )
+}
+
+export default SideMenu;
--- src/Components/index.js
+++ src/Components/index.js
@@ -1,3 +1,4 @@
 export {default as HomeGame} from './HomeGame/HomeGame'
 export {default as CourseSlide} from './CourseSlide/CourseSlide'
 export {default as FooterSlide} from './FooterSlide/FooterSlide'
+export {default as Menu} from "./SideMenu/SideMenu"

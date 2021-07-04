2b3c9106ae703bd2598ede9108fb9f679ec5e921
Neytllas <looopit@gmail.com>
2021-07-04 16:02:40 +0800

    сделаны страницы

--- src/Components/PracticeSlide/PracticeSlide.jsx
+++ src/Components/PracticeSlide/PracticeSlide.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import './practiceslide.css';
-// import en from "../../assets/en.jpg";
-// import forus from "../../assets/forus.jpg";
-// import gaz from "../../assets/gaz.jpg";
-// import irkut from "../../assets/irkut.jpg";
-// import isp from "../../assets/isp.jpg";
-// import krok from "../../assets/krok.jpg";
-// import sber from "../../assets/sber.png";
-// import vds from "../../assets/vds.jpg";
-// import yan from "../../../assets/yan.png";
+import en from "../../assets/en.png";
+import forus from "../../assets/forus.png";
+import gaz from "../../assets/gaz.png";
+import irkut from "../../assets/irkut.png";
+import isp from "../../assets/isp.png";
+import krok from "../../assets/krok.png";
+import sber from "../../assets/sber.png";
+import vds from "../../assets/vds.png";
+import yan from "../../assets/yan.png";
+import mts from "../../assets/mts.png";
+import adikt from "../../assets/adikt.png";
+import tele from "../../assets/tele.png";
 
 function PracticeSlide() 
 {
@@ -22,56 +25,61 @@ function PracticeSlide()
                 </div>
 
                 <div className="container">
-                        <div className="row justify-content-center mt-3 ml-3">
+                        <div className="row row--logo--company justify-content-center mt-3 ml-3">
                             <div className="col-4">
-                                {/* <img style="max-width: 100%;" src={yan}/> */}
-                                <img src="/assets/yan.png" alt="" />
+                                <img className="company--logo" src={yan}/> 
                             </div>
 
                             <div className="col-4">
-                                {/* <img style="max-width: 100%;" src={vds}/> */}
-                                <img src="/assets/vds.jpg" alt="" />
+                                <img className="company--logo" src={vds}/> 
                             </div>
 
                             <div className="col-4">
-                                {/* <img style="max-width: 100%;" src={sber}/> */}
-                                <img src="/assets/sber.png" alt="" />
+                                <img className="company--logo" src={sber}/>
+                            </div> 
+                        </div> 
+
+                        <div className="row row--logo--company justify-content-center mt-3 ml-3">
+                            <div className="col-4">
+                                 <img className="company--logo" src={krok}/>
+                            </div>
+
+                            <div className="col-4">
+                                 <img className="company--logo" src={en}/>
+                            </div>
+
+                            <div className="col-4">
+                                 <img className="company--logo" src={isp}/>
                             </div>
                         </div>
 
-                        <div className="row justify-content-center mt-3 ml-3">
+                        <div className="row row--logo--company justify-content-center mt-3 ml-3">
                             <div className="col-4">
-                                {/* <img style="max-width: 100%;" src={krok}/> */}
-                                <img src="/assets/krok.jpg" alt="" />
+                                 <img className="company--logo" src={irkut}/>
                             </div>
 
                             <div className="col-4">
-                                {/* <img style="max-width: 100%;" src={en}/> */}
-                                <img src="/assets/en.jpg" alt="" />
+                                 <img className="company--logo" src={gaz}/>
                             </div>
 
                             <div className="col-4">
-                                {/* <img style="max-width: 100%;" src={isp}/> */}
-                                <img src="/assets/isp.jpg" alt="" />
+                                 <img className="company--logo" src={forus}/>
                             </div>
                         </div>
 
-                        <div className="row justify-content-center mt-3 ml-3">
+                         <div className="row row--logo--company justify-content-center mt-3 ml-3">
                             <div className="col-4">
-                                {/* <img style="max-width: 100%;" src={irkut}/> */}
-                                <img src="/assets/irkut.jpg" alt="" />
+                                 <img className="company--logo" src={mts}/>
                             </div>
 
                             <div className="col-4">
-                                {/* <img style="max-width: 100%;" src={gaz}/> */}
-                                <img src="/assets/gaz.jpg" alt="" />
+                                 <img className="company--logo" src={tele}/>
                             </div>
 
                             <div className="col-4">
-                                {/* <img style="max-width: 100%;" src={forus}/> */}
-                                <img src="/assets/forus.jpg" alt="" />
-                            </div>
-                        </div>
+                                 <img className="company--logo" src={adikt}/>
+                            </div>  
+                        </div>  
                 </div>
 
             </div>
